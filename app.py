20b612f616f03d8def7fa4f5bc5177e72eb4b925
yuyan <yuyan@broue.cn>
2023-07-06 22:57:56 +0800

    1

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import json
-import random
 import smtplib
 from email.mime.text import MIMEText
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -10,22 +9,22 @@ from dotenv import load_dotenv
 # 加载环境变量
 load_dotenv()
 
-# 获取 API Key、备注和通知邮箱的列表
+# 获取用户ID、密码和API Key的列表
+user_ids = os.getenv("USER_ID").split(",")
+user_passwords = os.getenv("USER_PASSWORD").split(",")
 api_keys = os.getenv("API_KEYS").split(",")
-remarks = os.getenv("API_KEY_REMARKS").split(",")
-notification_emails = os.getenv("NOTIFICATION_EMAILS").split(",")
 
-# 设置 API 请求的 URL
-url = os.getenv("API_URL")
+# 设置API请求的URL
+url = "https://api.v2.rainyun.com"
 
-# 设置 SMTP 服务器和邮箱相关信息
+# 设置SMTP服务器和邮箱相关信息
 smtp_server = os.getenv("SMTP_SERVER")
 smtp_port = int(os.getenv("SMTP_PORT"))
 smtp_username = os.getenv("SMTP_USERNAME")
 smtp_password = os.getenv("SMTP_PASSWORD")
 sender_email = os.getenv("SENDER_EMAIL")
 
-# 设置 Headers
+# 设置Headers
 headers = {
     'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
     'Content-Type': 'application/json'
@@ -40,14 +39,28 @@ def create_email_content(remark, result):
     email_content['From'] = sender_email
     return email_content
 
+# 执行登录操作
+def perform_login(user_id, user_password):
+    payload = {
+        "field": user_id,
+        "password": user_password
+    }
+    response = requests.post(f"{url}/user/login", headers=headers, json=payload)
+    result = response.json()
+    if response.status_code == 200:
+        return result["x-api-key"]
+    else:
+        print(f"用户ID: {user_id} - 登录失败，错误信息: {result['message']}")
+        return None
+
 # 执行签到任务
 def perform_sign_in(api_key, remark, notification_email):
     headers['x-api-key'] = api_key
-    task_name = "每日签到"
+    task_name = os.getenv("TASK_NAME")
     payload = {
         "task_name": task_name
     }
-    response = requests.post(url, headers=headers, json=payload)
+    response = requests.post(f"{url}/user/reward/tasks", headers=headers, json=payload)
     result = response.json()
 
     if response.status_code == 200:
@@ -67,22 +80,26 @@ def send_email(notification_email, remark, result):
         smtp.sendmail(sender_email, notification_email, email_content.as_string())
         print("邮件通知已发送")
     except Exception as e:
-        print("发送邮件通知时出错:", str(e))
+        print(f"发送邮件通知时出错: {str(e)} - {notification_email} - {email_content.as_string()}")
+        # print("发送邮件通知时出错:", str(e))
     finally:
         if 'smtp' in locals():
             smtp.quit()
 
-# 遍历所有 API Key 签到
+# 遍历所有账号登录并签到
 def sign_in_all():
-    for i, api_key in enumerate(api_keys):
-        if i < len(remarks) and i < len(notification_emails):
-            remark = remarks[i]
-            notification_email = notification_emails[i]
-            perform_sign_in(api_key, remark, notification_email)
+    for i, user_id in enumerate(user_ids):
+        if i < len(api_keys):
+            user_password = user_passwords[i]
+            remark = f"User {user_id}"
+            notification_email = os.getenv("NOTIFICATION_EMAILS").split(",")[i]
+            api_key = perform_login(user_id, user_password)
+            if api_key:
+                perform_sign_in(api_key, remark, notification_email)
         else:
-            print(f"API Key: {api_key} - 未设置备注或通知邮箱")
+            print(f"用户ID: {user_id} - 未设置API Key")
 
-# 启动立刻执行一次
+# 启动立即执行一次
 sign_in_all()
 
 # 定时任务
