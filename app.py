3ba786aba1bb1bc04c2efe9e8b5583a271348c01
yuyan <yuyan@broue.cn>
2023-07-07 14:10:33 +0800

    优化错误处理

--- app.py
+++ app.py
@@ -2,30 +2,39 @@ import os
 import requests
 import json
 import smtplib
+import logging
 from email.mime.text import MIMEText
 from apscheduler.schedulers.blocking import BlockingScheduler
 from dotenv import load_dotenv
 
 # 加载环境变量
 load_dotenv()
+
+# 配置日志记录
+logging.basicConfig(filename='app.log', level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
+
 # 获取用户ID、密码和API Key的列表
 user_ids = os.getenv("USER_ID").split(",")
 user_passwords = os.getenv("USER_PASSWORD").split(",")
 api_keys = os.getenv("API_KEYS").split(",")
 api_key_remarks = os.getenv("API_KEY_REMARKS").split(",")
+
 # 设置API请求的URL
 url = "https://api.v2.rainyun.com"
+
 # 设置SMTP服务器和邮箱相关信息
 smtp_server = os.getenv("SMTP_SERVER")
 smtp_port = int(os.getenv("SMTP_PORT"))
 smtp_username = os.getenv("SMTP_USERNAME")
 smtp_password = os.getenv("SMTP_PASSWORD")
 sender_email = os.getenv("SENDER_EMAIL")
+
 # 设置Headers
 headers = {
     'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
     'Content-Type': 'application/json'
 }
+
 # 创建邮件内容
 def create_email_content(remark, result):
     subject = f"雨云签到结果 - {remark}"
@@ -46,8 +55,9 @@ def perform_login(user_id, user_password, remark):
     if response.status_code == 200:
         return result["x-api-key"]
     else:
-        print(f"用户ID: {user_id} -备注：{remark} - 登录失败，错误信息: {result['message']}")
-        send_email(os.getenv("NOTIFICATION_EMAILS").split(",")[user_ids.index(user_id)], f"用户ID: {user_id}", f"备注：{remark}", result)
+        error_message = result['message']
+        logging.error(f"用户ID: {user_id} - 备注：{remark} - 登录失败，错误信息: {error_message}")
+        send_email(os.getenv("NOTIFICATION_EMAILS").split(",")[user_ids.index(user_id)], f"用户ID: {user_id}", f"备注：{remark}", error_message)
         return None
 
 # 执行签到任务
@@ -64,39 +74,53 @@ def perform_sign_in(api_key, remark, notification_email, user_id):
         print(f"备注: {remark} - 用户id：{user_id} - 签到成功")
         send_email(notification_email, remark, result, user_id)
     else:
-        print(f"备注: {remark} - 用户id：{user_id} - 签到失败，错误信息: {result['message']}")
-        send_email(notification_email, remark, result, user_id)  
+        error_message = result['message']
+        logging.error(f"备注: {remark} - 用户id：{user_id} - 签到失败，错误信息: {error_message}")
+        send_email(notification_email, remark, result, user_id)
+
 # 发送邮件通知
 def send_email(notification_email, remark, result, user_id):
     try:
-        smtp = smtplib.SMTP_SSL(smtp_server, smtp_port)  
+        smtp = smtplib.SMTP_SSL(smtp_server, smtp_port)
         smtp.login(smtp_username, smtp_password)
         email_content = create_email_content(remark, result)
         smtp.sendmail(sender_email, notification_email, email_content.as_string())
         print(f"邮件通知已发送 - 用户ID: {user_id} - 邮件地址: {notification_email}")
     except Exception as e:
-        print(f"发送邮件通知时出错: {str(e)} - 用户ID: {user_id} - 邮件地址: {notification_email}")
+        error_message = str(e)
+        logging.error(f"发送邮件通知时出错: {error_message} - 用户ID: {user_id} - 邮件地址: {notification_email}")
     finally:
         if 'smtp' in locals():
-            smtp.quit()
-
+            try:
+                smtp.quit()
+            except Exception as e:
+                error_message = str(e)
+                logging.error(f"关闭SMTP连接时出错: {error_message}")
 
 # 遍历所有账号登录并签到
 def sign_in_all():
     for index, (user_id, user_password) in enumerate(zip(user_ids, user_passwords)):
         if user_id in user_ids and api_keys[user_ids.index(user_id)]:
-            remark = api_key_remarks[user_ids.index(user_id)] 
+            remark = api_key_remarks[user_ids.index(user_id)]
             notification_email = os.getenv("NOTIFICATION_EMAILS").split(",")[index] if index < len(os.getenv("NOTIFICATION_EMAILS").split(",")) else None
             api_key = api_keys[user_ids.index(user_id)]
             if api_key:
-                perform_sign_in(api_key, remark, notification_email, user_id)  
+                perform_sign_in(api_key, remark, notification_email, user_id)
         else:
             print(f"用户ID: {user_id} - 未设置API Key")
 
 # 启动立即执行一次
-sign_in_all()
+try:
+    sign_in_all()
+except Exception as e:
+    error_message = str(e)
+    logging.error(f"执行签到时出错: {error_message}")
 
 # 定时任务
 scheduler = BlockingScheduler()
-scheduler.add_job(sign_in_all, 'cron', hour=8, minute=0)  # 每天的8:00 AM触发签到任务
-scheduler.start()
+try:
+    scheduler.add_job(sign_in_all, 'cron', hour=8, minute=0)  # 每天的8:00 AM触发签到任务
+    scheduler.start()
+except Exception as e:
+    error_message = str(e)
+    logging.error(f"启动定时任务时出错: {error_message}")
