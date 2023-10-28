3d5d526cdffb8685379b5c27e3d953cc15179a29
散沙 <meng@broue.cn>
2023-10-28 13:30:14 +0800

    拟重构

---
+++ config.example.py
@@ -0,0 +1,13 @@
+# 用户id,用逗号分隔
+USER_ID=256411,12312
+# 用户密码，用逗号分隔，顺序与用户id对应
+USER_PASSWORD=ceshiPASSWORD1,ceshPASSWORD2
+# API Keys，用逗号分隔，顺序与用户id对应
+API_KEYS=Msu2EJy1rua,MsuzHNMrua
+# API Key 的备注，用逗号分隔，顺序与 API Keys 对应
+API_KEY_REMARKS=Remark1,Remark2
+# API Key 的通知邮箱，用逗号分隔，顺序与 API Keys 对应
+NOTIFICATION_EMAILS=ceshiemail@test.test,ceshiemail2@test.test
+
+# 签到任务名称
+TASK_NAME=每日签到
